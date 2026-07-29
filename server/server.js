e8275ccb6141a044b18f8e134675f090a7c571a7
Aayush Vyas <aayush25vyas@gmail.com>
2026-07-29 21:07:30 +0530

    product schema and model

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
-import databaseConnection from "#utils/db_connection/dbConnection.util";
 import express from "express";
+import setupCommonMiddleware from "#middleware/common/common.middleware";
+import setupErrorMiddleware from "#middleware/error/error.middleware";
+import databaseConnection from "#utils/db_connection/dbConnection.util";
 
 
-const app = express();
+const app = express(); 
+
+setupCommonMiddleware(app); 
+setupErrorMiddleware(app); 
 
 databaseConnection(app); 
